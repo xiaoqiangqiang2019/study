64b52b47c99db6f4bbb20e4499340998ee7870aa
xiaoqiangqiang2019 <79354550@qq.com>
2019-06-19 19:34:10 +0800

    6.19

--- 201906/6.19/test.py
+++ 201906/6.19/test.py
@@ -1,5 +1,4 @@
-from urllib import request, error
-try:
-    response = request.urlopen('http://cuiqingcai.com/index.htm')
-except error.URLError as e:
-    print(e.reason)
+import requests
+ 
+r = requests.get('http://httpbin.org/get')
+print(r.text)
